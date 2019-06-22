65800c2f2cb349dd441c596b2b0a72edf47a29e1
logue <logue@hotmail.co.jp>
2019-06-22 16:18:41 +0900

    Update version number.

--- MabinogiResource/resource.h
+++ MabinogiResource/resource.h
@@ -7,11 +7,11 @@
 #define IDS_WRITE_PACK_HEADER                   40002
 #define IDS_WRITE_FILE_LIST                     40003
 
-#define VER_FILEVERSION             1,0,7,1
-#define VER_FILEVERSION_STR         "1.0.7.1\0"
+#define VER_FILEVERSION             1,0,8,0
+#define VER_FILEVERSION_STR         "1.0.8.0\0"
 
-#define VER_PRODUCTVERSION          1,0,7,1
-#define VER_PRODUCTVERSION_STR      "1.0.7.1\0"
+#define VER_PRODUCTVERSION          1,0,8,0
+#define VER_PRODUCTVERSION_STR      "1.0.8.0\0"
 
 #ifndef DEBUG
 #define VER_DEBUG                   0
